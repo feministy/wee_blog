6e5ba9e42b9c43c840ae5272d06e5f7ccc5fc198
Liz Abinante <eabinante@gmail.com>
2013-06-06 10:53:00 -0500

    typo in times do syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 require 'faker'
 
-100 times do
+100.times do
   Post.create(title: Faker::Lorem.words(4).join(" "), content: Faker::Lorem.paragraphs(3))
 end
 
-20 times do
+20.times do
   Tag.create(name: Faker::Lorem.word)
 end 
 
 tags = Tag.all
 posts = Post.all
 
-300 times do
+300.times do
   Tagcloud.create(post_id: posts.sample, tag_id: tags.sample)
 end   
