f4b76afcb09b99471881e3268e9ea11bd37576bd
Liz Abinante <eabinante@gmail.com>
2013-06-06 13:49:04 -0500

    /posts/new get & post requests working

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,6 +7,12 @@ get '/posts/new' do
   erb :new
 end 
 
+get '/posts/:id/edit' do
+  id = params[:id]
+  @post = Post.find(id)
+  erb :edit
+end 
+
 get '/posts/:id' do
   id = params[:id]
   @post = Post.find(id)
@@ -14,4 +20,24 @@ get '/posts/:id' do
 end 
 
 post '/posts/new' do
+  @post = Post.new(params[:post])
+  if @post.save
+    redirect to '/'
+  else
+    erb :error
+  end 
+end 
+
+post '/posts/:id/edit' do
+  id = params[:id]
+  @post = Post.find(id)
+    if @post.update_attributes(params[:post])
+      redirect '/posts/:id'
+    else 
+      erb :error
+    end 
+end 
+
+not_found do
+ erb :error
 end 
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,21 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :tagclouds
   validates :content, :presence => true
   validates :title, :presence => true
+
+  before_save :tag_it
+
+  def tag_names
+    @tag_names
+  end
+
+  def tag_names=(tag_names)
+    @tag_names = tag_names
+  end
+
+  private
+  def tag_it
+    tags = @tag_names.split(',')
+    all_tags = tags.map { |tag| Tag.find_or_create_by_name(tag) }
+    self.tag_ids = all_tags.map(&:id)
+  end
 end
