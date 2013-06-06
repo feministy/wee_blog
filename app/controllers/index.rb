870e6a95bbfb079ae0c5b3338d84f772e57b662c
Liz Abinante <eabinante@gmail.com>
2013-06-06 12:41:56 -0500

    single post view

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,8 +3,15 @@ get '/' do
   erb :index
 end
 
+get '/posts/new' do
+  erb :new
+end 
+
 get '/posts/:id' do
   id = params[:id]
   @post = Post.find(id)
   erb :post
+end 
+
+post '/posts/new' do
 end 
